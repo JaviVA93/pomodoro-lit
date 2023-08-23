b3d240b5a236f5a8c06d36e50e61aef2c9d0ddae
Javi Villar <javiroll93@gmail.com>
2023-08-23 17:29:40 +0200

    change time feature, constant values

--- src/components/pomodoro.js
+++ src/components/pomodoro.js
@@ -1,48 +1,26 @@
 import { LitElement, html, css } from "lit";
 import animationInterval from "../utils/animationInterval";
+import { VIEWS, LEVELS } from "../utils/constants";
 
 class Pomodoro extends LitElement {
 
     static properties = {
         timer: 0,
         running: false,
+        initialValue: '',
+        view: '',
+        level: 0
     };
 
     constructor() {
         super()
 
-        this.timerInitValue = 60
-        this.restartCountdown()
         this.controller = new AbortController()
         this.running = false
+        this.level = LEVELS.MID
+        this.view = VIEWS.POMODORO
+        this.restartCountdown()
     }
-
-    static styles = css`
-        :host,
-        :host * {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-        }
-        :host {
-            font-size: 16px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 20px;
-            padding: 30px 60px;
-            border: 1px solid grey;
-            border-radius: 8px;
-        }
-        .buttonsWrapper {
-            display: flex;
-            align-items: center;
-            gap: 10px;
-        }
-        .buttonsWrapper button {
-            padding: 5px 15px;
-        }
-    `
 
     startCountdown() {
         if (this.running || this.timer <= 0)
@@ -55,7 +33,7 @@ class Pomodoro extends LitElement {
             this.timer--
 
             if (this.timer <= 0)
-                this.stopCountdown()            
+                this.stopCountdown()
         });
     }
 
@@ -69,19 +47,92 @@ class Pomodoro extends LitElement {
 
     restartCountdown() {
         this.stopCountdown()
-        this.timer = this.timerInitValue
+        this.timer = this.level
+    }
+
+    handleStartStopButton() {
+        (!this.running)
+            ? this.startCountdown()
+            : this.stopCountdown()
+    }
+
+    changeLevel(event) {
+        this.level = parseInt(event.target.value)
+        this.restartCountdown()
+    }
+
+    changeView() {
+        (this.view === VIEWS.POMODORO)
+            ? this.view = VIEWS.SETTINGS
+            : this.view = VIEWS.POMODORO
     }
 
     render() {
         return html`
-            <h3>Pomodoro</h3>
-            <h1>${this.timer}</h1>
-            <div class="buttonsWrapper">
-                <button @click=${() => this.startCountdown()}>${(!this.running) ? 'Start' : 'Pause'}</button>
-                <button @click=${() => this.restartCountdown()}>Restart</button>
-            </div>
+            <button
+                class="settingsBtn" 
+                @click=${() => this.changeView()}>
+               ${(this.view === VIEWS.POMODORO) ? 'Settings' : 'Back'}
+            </button>
+            ${(this.view === VIEWS.POMODORO)
+                ? html`
+                    <h3>Pomodoro</h3>
+                    <h1>${this.timer}</h1>
+                    <div class="buttonsWrapper">
+                        <button @click=${() => this.handleStartStopButton()}>
+                            ${(!this.running) ? 'Start' : 'Pause'}
+                        </button>
+                        <button @click=${() => this.restartCountdown()}>
+                            Restart
+                        </button>
+                    </div>`
+                : html`
+                    <label>
+                        Level
+                        <select @change=${(e) => this.changeLevel(e)}>
+                            <option value="${LEVELS.LOW}">Low (10min)</option>
+                            <option value="${LEVELS.MID}">Medium (20min)</option>
+                            <option value="${LEVELS.HIGH}">High (40min)</option>
+                        </select>
+                    </label>
+                `
+            }
         `
     }
+
+    static styles = css`
+        :host,
+        :host * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+        :host {
+            position: relative;
+            font-size: 16px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 20px;
+            padding: 30px 60px;
+            border: 1px solid grey;
+            border-radius: 8px;
+        }
+        .settingsBtn {
+            padding: 5px;
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+        .buttonsWrapper {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+        .buttonsWrapper button {
+            padding: 5px 15px;
+        }
+    `
 }
 
 
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const VIEWS = {
+    POMODORO: 'POMO',
+    SETTINGS: 'SETTINGS'
+}
+
+export const LEVELS = {
+    LOW: 10,
+    MID: 20,
+    HIGH: 40
+}
